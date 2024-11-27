928fbc4b6557a7389bf8b7de5e5dc67e2197a8c7
dogrento <dogrento93@gmail.com>
2024-11-27 20:46:36 -0300

    testando compilacao pos intalacao do SMFL. Renderizando um circulo verde

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# tec_prog_c-_game
+# tec_prog_c++_game
 Repositorio referente ao projeto proposto pela Disciplina Tecnicas de Programacao da UTFPR.
+
+### Compilacao até aqui.
+
+```g++ -c main.cpp -I /usr/include``` 
+```g++ main.o -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system``` 
--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 #include <stdlib.h>
+#include <string>
 using namespace std;
 
-int main(void){
+
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
     return 0;
 }
